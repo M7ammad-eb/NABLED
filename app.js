17d2a5e9fd857f8d7115e8a754a865e41f004a74
Mohammad Eb <M7ammad.eb@gmail.com>
2025-02-26 01:30:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase Configuration (Replace with your real Firebase keys)
+const firebaseConfig = {
+  apiKey: "AIzaSyAzgx1Ro6M7Bf58dgshk_7Eflp-EtZc9io",
+  authDomain: "nab-led.firebaseapp.com",
+  projectId: "nab-led",
+  storageBucket: "nab-led.firebasestorage.app",
+  messagingSenderId: "789022171426",
+  appId: "1:789022171426:web:2d8dda594b1495be26457b",
+  measurementId: "G-W58SF16RJ6"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAzgx1Ro6M7Bf58dgshk_7Eflp-EtZc9io",
-    authDomain: "nab-led.firebaseapp.com",
-    projectId: "nab-led",
-    storageBucket: "nab-led.firebasestorage.app",
-    messagingSenderId: "789022171426",
-    appId: "1:789022171426:web:2d8dda594b1495be26457b",
-    measurementId: "G-W58SF16RJ6"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
 // Google Sign-In Button
 window.onload = function () {
   google.accounts.id.initialize({
-    client_id: "YOUR_GOOGLE_CLIENT_ID", // Replace with your Google Client ID
+    client_id: "m7ammad.eb@gmail.com",
     callback: handleCredentialResponse
   });
 

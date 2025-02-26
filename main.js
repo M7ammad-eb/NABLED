adc98bd65ca6cf8228b32bf4351192b996fe1e6d
Mohammad Eb <M7ammad.eb@gmail.com>
2025-02-26 22:27:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,6 @@ fetch("https://script.google.com/macros/s/AKfycbwB2p9HSEbNrObDDh_0Hmg3MWDfPjyJ2d
 })
 .then(res => res.json())
 .then(res => console.log(res));
-.catch(error => console.error("Error fetching data:", error));
 
 
 

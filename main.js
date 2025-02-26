f81de15e234204bb924f6bf8138ef272598211c2
Mohammad Eb <M7ammad.eb@gmail.com>
2025-02-27 00:20:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ if (signOutButton) {
 
 
 // Fetsh Google sheets data
-fetch("https://script.google.com/macros/s/AKfycbysGgGsp5L9hY5Hj1IcdosM_nHFIIfoJMzn6hVP_2IfJ6vxCW8hOCz3BdKEIyX_d6fN9A/exec", {
+fetch("https://script.google.com/macros/s/AKfycbyxxp42FAt73VYqCzE1r7DiVDyscilJ8NbRi2B-SKj23aIxwnMuTGdtOQO7NIg1FwcFwQ/exec", {
   method: "GET",
   headers: {
     "Content-Type": "application/json"
